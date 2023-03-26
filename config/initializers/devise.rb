0323bda4a99aa671c3a2f704f1a2a54620f14d40
Eray Alkış <lolpo12345@outlook.com>
2023-03-26 21:20:13 +0300

    Remove debug puts call for jwt secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,7 +311,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  puts Rails.application.credentials.fetch(:devise_jwt_secret_key)
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.fetch(:devise_jwt_secret_key)
 
