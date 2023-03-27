adbbd8f8bdc58f42a8095910ce7d5a505d6bb8c9
Eray Alkış <lolpo12345@outlook.com>
2023-03-27 22:15:25 +0300

    Add username to devise model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::API
   respond_to :json
   before_action :authenticate_user!, only: [:return_current_user]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def return_current_user
     render json: current_user
   end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_26_145038) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_191405) do
   create_table "blocks", force: :cascade do |t|
     t.integer "spawn_id", null: false
     t.string "substrate", null: false
@@ -57,8 +57,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_145038) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
