d70bde8be7d04fd49b8e5478bb5b64152f2b02b6
Eray Alkış <lolpo12345@outlook.com>
2023-04-20 01:50:21 +0300

    Update production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 end
